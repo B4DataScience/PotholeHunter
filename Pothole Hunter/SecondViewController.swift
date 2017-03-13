58a4f18a7df2d7f362b559c30ae8ff085ba959a3
Brijesh Patel <brijeshpatel@brijeshs-macbook-air.local>
2017-03-13 17:47:30 -0600

    Initial Commit

---
+++ Pothole Hunter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Pothole Hunter
+//
+//  Created by Brijesh Patel on 2017-03-13.
+//  Copyright © 2017 Brijesh Patel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
